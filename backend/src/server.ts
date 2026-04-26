f0bf817a28bf5f11d04c1b629e80d9819cddbfca
Robert Tagliavia <rtagliavia.pp@gmail.com>
2026-04-26 12:04:19 -0400

    add: client url to socket origin list
    chore: moved socket event handlers  to ./socket/handlers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import { Server as SocketServer } from 'socket.io';
 import config from './config';
 import { startEventListeners } from './services/eventListener';
 import { initializeDatabase } from './db/init';
-import { SOCKET_EVENTS } from './constants/socketEvents';
+import { registerSocketHandlers } from './socket/handlers';
 
 export async function startServer() {
   await initializeDatabase();
@@ -12,19 +12,14 @@ export async function startServer() {
   const server = http.createServer(app);
   const io = new SocketServer(server, {
     cors: {
-      origin: config.clientUrl,
+      origin: [config.clientUrl as string],
       methods: ['GET', 'POST'],
     },
   });
 
   (global as any).io = io;
 
-  io.on(SOCKET_EVENTS.CONNECTION, (socket) => {
-    console.log('New client connected:', socket.id);
-    socket.on(SOCKET_EVENTS.DISCONNECT, () => {
-      console.log('Client disconnected:', socket.id);
-    });
-  });
+  registerSocketHandlers(io);
 
   startEventListeners();
 
