8c4ba41cbd2fb6ce1bd4ac6bed54657375b9c386
Robert Tagliavia <rtagliavia.pp@gmail.com>
2026-04-26 11:55:22 -0400

    socket events

--- backend/src/constants/socketEvents.ts
+++ backend/src/constants/socketEvents.ts
@@ -1,6 +1,7 @@
 export const SOCKET_EVENTS = {
   CONNECTION: 'connection',
   DISCONNECT: 'disconnect',
+  ORDER_CODES: 'order-codes',
   ORDER_PAID: 'order-paid',
   CANCEL_ORDER: 'cancel-order',
   ORDER_CANCELLED: 'order-cancelled'
