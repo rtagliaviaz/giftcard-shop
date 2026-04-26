c5307bdaffbb4eaa45c34692dc21893856199f2b
Robert Tagliavia <rtagliavia.pp@gmail.com>
2026-04-26 12:09:45 -0400

    add: constants for api endpoints
    add: new routes for giftcards related endpoints
    
    add: rate limiter middleware

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
-import { createOrder, getOrderStatus } from '../controllers/orderController';
-
+import { createOrder, getOrderStatus, getOrderCodes } from '../controllers/orderController';
+import {getGiftCards, getGiftCardTypes } from '../controllers/giftCardsController';
+import { API_ROUTES } from '../constants/apiRoutes';
+import { orderInfoLimiter } from '../middleware/rateLimiter';
 const router = Router();
 
-router.get('/health', (req, res) => {
+router.get(API_ROUTES.HEALTH, (req, res) => {
   res.json({ status: 'ok' });
 });
-router.post('/create-order', createOrder);
-router.get('/order-status/:uid', getOrderStatus);
-// more routes: /order-code/:id, /cancel-order/:id, etc.
+router.post(API_ROUTES.CREATE_ORDER, createOrder);
+router.get(API_ROUTES.GET_ORDER_STATUS, orderInfoLimiter, getOrderStatus);
+router.get(API_ROUTES.GET_ORDER_CODES,  orderInfoLimiter, getOrderCodes);
+router.get(API_ROUTES.GET_GIFT_CARDS, getGiftCards);
+router.get(API_ROUTES.GET_GIFT_CARD_TYPES, getGiftCardTypes);
 
 export default router;
