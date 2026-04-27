dc12ee2252b9ea1b8136415ceff074c09da5471d
Robert Tagliavia <rtagliavia.pp@gmail.com>
2026-04-27 08:38:18 -0400

    feat: added GET_GIFT_CARD_TYPE_BY_ID
    chore: removed GET_GIFT_CARDS

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { createOrder, getOrderStatus, getOrderCodes } from '../controllers/orderController';
-import {getGiftCards, getGiftCardTypes } from '../controllers/giftCardsController';
+import { getGiftCardTypes, getGiftCardTypeById } from '../controllers/giftCardsController';
 import { API_ROUTES } from '../constants/apiRoutes';
 import { orderInfoLimiter } from '../middleware/rateLimiter';
 const router = Router();
@@ -11,7 +11,8 @@ router.get(API_ROUTES.HEALTH, (req, res) => {
 router.post(API_ROUTES.CREATE_ORDER, createOrder);
 router.get(API_ROUTES.GET_ORDER_STATUS, orderInfoLimiter, getOrderStatus);
 router.get(API_ROUTES.GET_ORDER_CODES,  orderInfoLimiter, getOrderCodes);
-router.get(API_ROUTES.GET_GIFT_CARDS, getGiftCards);
 router.get(API_ROUTES.GET_GIFT_CARD_TYPES, getGiftCardTypes);
+router.get(API_ROUTES.GET_GIFT_CARD_TYPE_BY_ID, getGiftCardTypeById);
+
 
 export default router;
