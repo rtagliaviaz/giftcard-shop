5ee6eec0978f6efcec9138aab98ec5daf27b2c2d
Robert Tagliavia <rtagliavia.pp@gmail.com>
2026-04-27 08:33:38 -0400

    feat: added /gift-card-types/:id

--- backend/src/constants/apiRoutes.ts
+++ backend/src/constants/apiRoutes.ts
@@ -5,4 +5,5 @@ export const API_ROUTES = {
   GET_ORDER_CODES: '/order-codes/:uid',
   GET_GIFT_CARDS: '/gift-cards',
   GET_GIFT_CARD_TYPES: '/gift-card-types',
+  GET_GIFT_CARD_TYPE_BY_ID: '/gift-card-types/:id',
 }
