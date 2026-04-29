dc371e66efeed17edf0974420fdf67487e7a0446
Robert Tagliavia <rtagliavia.pp@gmail.com>
2026-04-29 14:21:38 -0400

    feat: added SweepLog
    chore: removed GiftCardInventory
    
    chore: removed PaymentTransactions

--- backend/src/entity/GiftCardDatabase.ts
+++ backend/src/entity/GiftCardDatabase.ts
@@ -1,20 +1,18 @@
 import { GiftCard } from "./GiftCard";
-import { GiftCardInventory } from "./GiftCardInventory";
 import { Orders } from "./Orders";
 import { OrderItems } from "./OrderItems";
 import { GiftCardCodes } from "./GiftCardCodes";
-import {PaymentTransactions} from "./PaymentTransactions";
 import {Settings} from "./Settings";
 import { GiftCardType } from "./GiftCardType";
+import { SweepLog } from "./SweepLog";
 
 
 export {
     GiftCard,
-    GiftCardInventory,
     Orders,
     OrderItems,
     GiftCardCodes,
-    PaymentTransactions,
     Settings,
     GiftCardType,
+    SweepLog,
 }
