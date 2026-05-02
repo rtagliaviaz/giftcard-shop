58ff6a9ee3c54c64fc9112eaee99d4740f3c7853
Robert Tagliavia <rtagliavia.pp@gmail.com>
2026-05-02 16:27:18 -0400

    chore: added /cancel-order/:uid as CANCEL_ORDER

--- frontend/src/constants/apiRoutes.ts
+++ frontend/src/constants/apiRoutes.ts
@@ -1,5 +1,6 @@
 export const API_ROUTES = {
   CREATE_ORDER: '/create-order',
+  CANCEL_ORDER: (uid: string) => `/cancel-order/${uid}`,
   ORDER_STATUS: (uid: string) => `/order-status/${uid}`,
   ORDER_CODES: (uid: string) => `/order-codes/${uid}`,
   GIFT_CARD_TYPES: '/gift-card-types',
